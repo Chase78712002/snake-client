f5604df5097586bc013c4fa41cb938ebd98197c3
Alvin Lin <alvinch.lin@gmail.com>
2021-04-07 21:55:18 +0000

    created the setupInput function to configure our stdin. first set it to RawMode that allows input be available character by character but it also removes the ctrl+C way of exit. So we also created a function to handleUserInput to deal with that. we then set the encoding to utf8 and setup the resume method to prevent the process from exiting

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const net = require('net');
 const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = () =>{
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  })
+  return stdin;
+}
+
+setupInput();
