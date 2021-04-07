bf86b0ddb138c179d122984e13c82c9471882c03
Alvin Lin <alvinch.lin@gmail.com>
2021-04-07 20:26:49 +0000

    moved the connect function to client.js. exports the connect function as a key:value pair in an object. import the connect function inside the client.js in play.js. called the connect function in play.js. All worked out

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
 // This is our main file to launch the game client
 const net = require('net');
-
-const connect = () => {
-  const client = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
-
-  client.setEncoding('utf8');
-
-  client.on('data', (message) => {
-    console.log(`Server said ${message}`);
-  })
-
-  client.on('connect', () => {
-    console.log("You're now connected to the Server!");
-  })
-
-  return client;
-}
-
+const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
