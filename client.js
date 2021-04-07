0b4669405996d5002e9ad94e49682000457b1e54
Alvin Lin <alvinch.lin@gmail.com>
2021-04-07 21:23:25 +0000

    hard-coded moving commands using setTimeout and setInterval

--- client.js
+++ client.js
@@ -16,6 +16,22 @@ const connect = () => {
     client.write("Name: ALV");
   })
 
+  // Hard coded moving commands===============
+  // client.on('connect', () => {
+  //   let time = 0;
+  //   // while(time < 5000) {
+  //   //   setTimeout(() => {
+  //   //     client.write("Move: up");
+  //   //   }, time);
+  //   //   time += 50;
+  //   // let moveLeftInterval = setInterval(() => {
+  //   //   client.write("Move: left");
+  //   //   time += 500;
+  //   //   if (time === 5000) {
+  //   //     clearInterval(moveLeftInterval);
+  //   //   }
+  //   // }, 500);
+  // })
 
 
   return client;
