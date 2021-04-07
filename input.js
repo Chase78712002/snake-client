30f5ae4c4d8399e1f691c16e16bd27dfc9247b05
Alvin Lin <alvinch.lin@gmail.com>
2021-04-07 23:22:05 +0000

    added a client parameter for setupInput function so that it can leverage the returned client from the connect function. takes in client as argument and uses it to send message to server. these messages are setup as movement commands corresponding to w,s,a,d. Sucessfully control the movement of the snake using the four keys set up

--- input.js
+++ input.js
@@ -4,13 +4,23 @@ const handleUserInput = (key) => {
   }
 }
 
-const setupInput = () =>{
+let connection;
+
+
+const setupInput = (client) =>{
+  connection = client;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
     handleUserInput(key);
+    switch(key) {
+      case "w": connection.write("Move: up"); break;
+      case "s": connection.write("Move: down"); break;
+      case "a": connection.write("Move: left"); break;
+      case "d": connection.write("Move: right"); break;
+    }
   })
   return stdin;
 }
